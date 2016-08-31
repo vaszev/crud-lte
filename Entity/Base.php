37b162c51a0c50095ad04e3290cfa331d785eb4d
rbodnar <r.bodnar@pearwilliams.com>
2016-08-31 09:06:44 +0200

    - switchDeletedFilter added to Base class

--- Entity/Base.php
+++ Entity/Base.php
@@ -1,177 +1,210 @@
 <?php
 
-namespace Vaszev\CrudLteBundle\Entity;
+  namespace Vaszev\CrudLteBundle\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
-use Doctrine\ORM\Mapping\GeneratedValue;
-use Doctrine\ORM\Mapping\HasLifecycleCallbacks;
-use Doctrine\ORM\Mapping\MappedSuperclass;
+  use Doctrine\ORM\Mapping as ORM;
+  use Doctrine\ORM\Mapping\GeneratedValue;
+  use Doctrine\ORM\Mapping\HasLifecycleCallbacks;
+  use Doctrine\ORM\Mapping\MappedSuperclass;
 
 //use Knp\DoctrineBehaviors\Model as ORMBehaviors;
 
-/**
- * @MappedSuperclass
- * @HasLifecycleCallbacks
- */
-class Base implements SoftDeleteInterface {
-
   /**
-   * @ORM\Id
-   * @ORM\Column(type="integer")
-   * @GeneratedValue(strategy="AUTO")
+   * @MappedSuperclass
+   * @HasLifecycleCallbacks
    */
-  protected $id;
-  /**
-   * @ORM\Column(type="datetime", nullable=true)
-   */
-  protected $deleted;
-  /**
-   * @ORM\Column(type="datetime")
-   */
-  protected $created;
-  /**
-   * @ORM\Column(type="datetime")
-   */
-  protected $edited;
+  class Base implements SoftDeleteInterface {
 
-  /**
-   * @ORM\Column(name="is_deleted", type="integer")
-   */
-  private $isDeleted = 0;
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    protected $deleted;
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $created;
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $edited;
 
-
-
-  public function __construct() {
-    if ($this->isNew()) {
-      $this->created = $this->edited = new \DateTime ();
-    }
-  }
-
-
-
-  /**
-   * Get the entity id
-   * @return int
-   */
-  public function getId() {
-    return ( int )$this->id;
-  }
+    /**
+     * @ORM\Column(name="is_deleted", type="integer")
+     */
+    private $isDeleted = 0;
 
 
 
-  /**
-   * Get the time of deletion
-   * @return \DateTime|null
-   */
-  public function getDeleted() {
-    return $this->deleted;
-  }
-
-
-
-  /**
-   * Get the time of creation
-   * @return \DateTime
-   */
-  public function getCreated() {
-    return $this->created;
-  }
-
-
-
-  /**
-   * Get the time of edition
-   * @return \DateTime
-   */
-  public function getEdited() {
-    return $this->edited;
-  }
-
-
-
-  /**
-   * Check if the entity is new
-   * @return bool
-   */
-  public function isNew() {
-    return (empty ($this->id));
-  }
-
-
-
-  /**
-   * Set the time of deletion
-   * @param \DateTime $deleted
-   */
-  public function setDeleted(\DateTime $deleted = null) {
-    $this->deleted = $deleted;
-    $this->isDeleted = intval(!empty($deleted));
-
-    return $this;
-  }
-
-
-
-  /**
-   * @return mixed
-   */
-  public function getIsDeleted() {
-    return $this->isDeleted;
-  }
-
-
-
-  /**
-   * Set the time of creation
-   * @param \DateTime $created
-   */
-  public function setCreated(\DateTime $created) {
-    $this->created = $created;
-
-    return $this;
-  }
-
-
-
-  /**
-   * Set the time of edition
-   * @param \DateTime $edited
-   */
-  public function setEdited(\DateTime $edited) {
-    $this->edited = $edited;
-
-    return $this;
-  }
-
-
-
-  /**
-   * Set the time of edition
-   * @param \DateTime $edited
-   * @ORM\PreUpdate
-   */
-  public function updateEdited() {
-    $this->edited = new \DateTime();
-  }
-
-
-
-  /**
-   * Bind data
-   * @param array $array
-   */
-  public function bind($array) {
-    $ref = new \ReflectionClass ($this);
-    $pros = $ref->getDefaultProperties();
-    foreach (array_intersect_key($array, $pros) as $key => $value) {
-      if (is_string($value)) {
-        $value = trim($value);
+    public function __construct() {
+      if ($this->isNew()) {
+        $this->created = $this->edited = new \DateTime ();
       }
-      $method = "set" . ucfirst($key);
-      $ref->hasMethod($method) ? $this->{$method} ($value) : $this->{$key} = $value;
     }
 
-    return $this;
-  }
 
-}
+
+    /**
+     * Get the entity id
+     * @return int
+     */
+    public function getId() {
+      return ( int )$this->id;
+    }
+
+
+
+    /**
+     * Get the time of deletion
+     * @return \DateTime|null
+     */
+    public function getDeleted() {
+      return $this->deleted;
+    }
+
+
+
+    /**
+     * Get the time of creation
+     * @return \DateTime
+     */
+    public function getCreated() {
+      return $this->created;
+    }
+
+
+
+    /**
+     * Get the time of edition
+     * @return \DateTime
+     */
+    public function getEdited() {
+      return $this->edited;
+    }
+
+
+
+    /**
+     * Check if the entity is new
+     * @return bool
+     */
+    public function isNew() {
+      return (empty ($this->id));
+    }
+
+
+
+    /**
+     * Set the time of deletion
+     * @param \DateTime $deleted
+     */
+    public function setDeleted(\DateTime $deleted = null) {
+      $this->deleted = $deleted;
+      $this->isDeleted = intval(! empty($deleted));
+
+      return $this;
+    }
+
+
+
+    /**
+     * @return mixed
+     */
+    public function getIsDeleted() {
+      return $this->isDeleted;
+    }
+
+
+
+    /**
+     * Set the time of creation
+     * @param \DateTime $created
+     */
+    public function setCreated(\DateTime $created) {
+      $this->created = $created;
+
+      return $this;
+    }
+
+
+
+    /**
+     * Set the time of edition
+     * @param \DateTime $edited
+     */
+    public function setEdited(\DateTime $edited) {
+      $this->edited = $edited;
+
+      return $this;
+    }
+
+
+
+    /**
+     * Set the time of edition
+     * @param \DateTime $edited
+     * @ORM\PreUpdate
+     */
+    public function updateEdited() {
+      $this->edited = new \DateTime();
+    }
+
+
+
+    /**
+     * Bind data
+     * @param array $array
+     */
+    public function bind($array) {
+      $ref = new \ReflectionClass ($this);
+      $pros = $ref->getDefaultProperties();
+      foreach (array_intersect_key($array, $pros) as $key => $value) {
+        if (is_string($value)) {
+          $value = trim($value);
+        }
+        $method = "set" . ucfirst($key);
+        $ref->hasMethod($method) ? $this->{$method} ($value) : $this->{$key} = $value;
+      }
+
+      return $this;
+    }
+
+
+
+    /**
+     * @param bool   $turnOff
+     * @param string $id
+     * @param bool   $exception
+     * @return null
+     * @throws \Exception
+     */
+    protected function switchDeletedFilter($turnOff = false, $id = "default", $exception = false) {
+      static $baseState = [];
+      if ( ! property_exists($this, 'em')) {
+        if ($exception) {
+          throw new \Exception('EntityManager does not initialized.');
+        } else {
+          return null;
+        }
+      }
+      if ( ! array_key_exists($id, $baseState)) {
+        // save state at first time
+        $baseState[$id] = $this->em->getFilters()->isEnabled('not_deleted');
+      }
+      $filterOn = $this->em->getFilters()->isEnabled('not_deleted');
+      if ($turnOff && $filterOn) {
+        // turn off now
+        $this->em->getFilters()->disable('not_deleted');
+      }
+      if ( ! $turnOff && $baseState[$id] && ! $filterOn) {
+        // turn on back
+        $this->em->getFilters()->enable('not_deleted');
+      }
+    }
+
+  }
